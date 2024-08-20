5c2892c31a0eae2883b0063d27fece8977b1b315
Hellen Lima Araujo <202415020015@ad.ifpb.edu.br>
2024-08-20 20:12:46 -0300

    feat: janela inicial

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+largura = 1920
+altura = 1080
+loop = True
+imagem_fundo = pygame.image.load('Wander-Warriors/src/cenario.png')
+janela = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption('Wander Warriors')
+
+while loop:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      loop = False
+  pygame.display.update()
+  janela.blit(imagem_fundo, (0,0))
