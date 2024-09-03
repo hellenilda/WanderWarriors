cd6733ac89ed9f915d9667b5b09f67833529dc24
hellenilda <hellen95limaaraujo@gmail.com>
2024-09-03 13:09:52 -0300

    feat: novas sprites de NPCs

--- main.py
+++ main.py
@@ -22,18 +22,14 @@ def draw_text_box(surface, text, position, width, height):
     font = pygame.font.Font(None, 36)
     text_surf = font.render(text, True, pygame.Color('white'))
     text_rect = text_surf.get_rect(center=position)
-
-    # Desenho da caixa de texto
     box_rect = pygame.Rect(0, 0, width, height)
     box_rect.center = position
 
     pygame.draw.rect(surface, pygame.Color('black'), box_rect)
-    pygame.draw.rect(surface, pygame.Color('white'), box_rect, 2)  # Bordas brancas
-
-    # Desenho do texto na caixa
+    pygame.draw.rect(surface, pygame.Color('white'), box_rect, 2)
     surface.blit(text_surf, text_rect)
 
-# Função para criar sprite
+# Função para criar um sprite
 def create_sprite(image_path, pos, group, size=None):
     sprite = pygame.sprite.Sprite(group)
     sprite.image = load_and_scale_image(image_path, size) if size else pygame.image.load(image_path).convert_alpha()
@@ -41,8 +37,8 @@ def create_sprite(image_path, pos, group, size=None):
     return sprite
 
 # Função para verificar colisões de limite
-def check_boundary(player, scenario_rect):
-    player.rect.clamp_ip(scenario_rect)
+def check_boundary(rect, boundary_rect):
+    rect.clamp_ip(boundary_rect)
 
 # Função para criar o jogador
 def create_player(pos, group, obstacle_sprites, frames):
@@ -57,35 +53,21 @@ def create_player(pos, group, obstacle_sprites, frames):
     player.obstacle_sprites = obstacle_sprites
     return player
 
-# Função para entrada de movimento do jogador
-def player_input(player, wanderley_up, wanderley_down, wanderley_right):
+# Função para atualizar a entrada do jogador
+def update_player_input(player, wanderley_up, wanderley_down, wanderley_right):
     keys = pygame.key.get_pressed()
-    player.direction.x = 0
-    player.direction.y = 0
+    player.direction.x, player.direction.y = 0, 0
 
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
         player.direction.y = -1
         player.frames = wanderley_up
-    elif keys[pygame.K_DOWN]:
+    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
         player.direction.y = 1
         player.frames = wanderley_down
-    elif keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player.direction.x = 1
         player.frames = wanderley_right
-    elif keys[pygame.K_LEFT]:
-        player.direction.x = -1
-        player.frames = [pygame.transform.flip(frame, True, False) for frame in wanderley_right]
-
-    elif keys[pygame.K_w]:
-        player.direction.y = -1
-        player.frames = wanderley_up
-    elif keys[pygame.K_s]:
-        player.direction.y = 1
-        player.frames = wanderley_down
-    elif keys[pygame.K_d]:
-        player.direction.x = 1
-        player.frames = wanderley_right
-    elif keys[pygame.K_a]:
+    elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
         player.direction.x = -1
         player.frames = [pygame.transform.flip(frame, True, False) for frame in wanderley_right]
 
@@ -99,27 +81,27 @@ def animate_player(player):
         player.frame_index = 1  # Frame de idle
     player.image = player.frames[int(player.frame_index)]
 
-# Função para mover o jogador
+# Função para mover o jogador e lidar com colisões
 def move_player(player, speed, scenario_rect):
     player.rect.x += player.direction.x * speed
-    check_collision(player, 'horizontal')
+    handle_collision(player, 'horizontal')
     player.rect.y += player.direction.y * speed
-    check_collision(player, 'vertical')
-    check_boundary(player, scenario_rect)
+    handle_collision(player, 'vertical')
+    check_boundary(player.rect, scenario_rect)
 
 # Função para verificar colisão com obstáculos
-def check_collision(player, direction):
+def handle_collision(player, direction):
     for sprite in player.obstacle_sprites:
         if sprite.rect.colliderect(player.rect):
             if direction == 'horizontal':
-                if player.direction.x > 0:  # Movendo para a direita
+                if player.direction.x > 0:
                     player.rect.right = sprite.rect.left
-                elif player.direction.x < 0:  # Movendo para a esquerda
+                elif player.direction.x < 0:
                     player.rect.left = sprite.rect.right
             elif direction == 'vertical':
-                if player.direction.y > 0:  # Movendo para baixo
+                if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
-                elif player.direction.y < 0:  # Movendo para cima
+                elif player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
 
 # Função para criar o grupo da câmera e configurar a superfície de exibição
@@ -129,10 +111,10 @@ def create_camera_group(scenario_rect, cenario):
     group.offset = pygame.math.Vector2()
     group.half_w, group.half_h = [dim // 2 for dim in group.display_surface.get_size()]
     group.camera_borders = {'left': 200, 'right': 200, 'top': 100, 'bottom': 100}
-    
+
     l, t = group.camera_borders['left'], group.camera_borders['top']
     w, h = group.display_surface.get_size()
-    w -= group.camera_borders['left'] + group.camera_borders['right']
+    w -= group.camera_borders['left'] +     group.camera_borders['right']
     h -= group.camera_borders['top'] + group.camera_borders['bottom']
     group.camera_rect = pygame.Rect(l, t, w, h)
 
@@ -149,17 +131,17 @@ def create_camera_group(scenario_rect, cenario):
 
     group.max_x = scenario_rect.width - group.display_surface.get_width()
     group.max_y = scenario_rect.height - group.display_surface.get_height()
-    
+
     return group
 
 # Função para centralizar a câmera no alvo (player)
-def center_target_camera(camera_group, target):
+def center_camera(camera_group, target):
     camera_group.offset.x = min(max(target.rect.centerx - camera_group.half_w, 0), camera_group.max_x)
     camera_group.offset.y = min(max(target.rect.centery - camera_group.half_h, 0), camera_group.max_y)
 
 # Função para desenhar elementos na tela usando a câmera
 def custom_draw(camera_group, player):
-    center_target_camera(camera_group, player)
+    center_camera(camera_group, player)
     camera_group.internal_surf.fill('#71ddee')
 
     ground_offset = camera_group.ground_rect.topleft - camera_group.offset + camera_group.internal_offset
@@ -176,10 +158,8 @@ def main():
     pygame.init()
     screen, clock = initialize_screen()
 
-    global cenario, wanderley_down, wanderley_up, wanderley_right
+    # Carregar o cenário e as animações
     cenario = pygame.image.load('Sprites/ifpb.jpg').convert_alpha()
-    
-    # Carregar os frames de animação do jogador
     wanderley_down = load_animation_frames('Sprites/personagens/Vander/Baixo/Sprite-baixo-', 3, (64, 64))
     wanderley_up = load_animation_frames('Sprites/personagens/Vander/Cima/Sprite-cima-', 3, (64, 64))
     wanderley_right = load_animation_frames('Sprites/personagens/Vander/Lados/Sprite-lados-', 3, (64, 64))
@@ -188,10 +168,10 @@ def main():
     camera_group = create_camera_group(scenario_rect, cenario)
     obstacle_sprites = pygame.sprite.Group()
 
-    # Criação do jogador
+    # Criar o jogador
     player = create_player((1350, 370), camera_group, obstacle_sprites, wanderley_down)
 
-    # Adicionando obstáculos
+    # Adicionar obstáculos
     sprites_info = [
         ('Sprites/colisores/bloco-adm.png', (0, 0), (1210, 500)),
         ('Sprites/colisores/estacionamento.png', (130, 785), (378, 257)),
@@ -201,28 +181,26 @@ def main():
         ('Sprites/colisores/lampada-2.png', (1090, 790), (24, 95)),
         ('Sprites/colisores/lampada-2.png', (1330, 590), (24, 95))
     ]
-
     for path, pos, size in sprites_info:
         create_sprite(path, pos, obstacle_sprites, size)
 
-    # Adicionando sprites de fundo diretamente
+    # Adicionar sprites de fundo
     fundo_info = [
-        ('Sprites/colisores/arvore.png', (1292, 799), (97,101)),
+        ('Sprites/colisores/arvore.png', (1292, 799), (97, 101)),
         ('Sprites/colisores/arco.png', (1165, 983), (355, 132)),
         ('Sprites/colisores/estacionamento-arbustos.png', (512, 768), (149, 91)),
         ('Sprites/colisores/rampa.png', (1210, 0), (390, 372))
     ]
-
-    # Criar e adicionar sprites de fundo ao grupo da câmera
     for path, pos, size in fundo_info:
         create_sprite(path, pos, camera_group, size)
 
-    # Instanciação dos NPCs com os sprites correspondentes
-    npc1 = create_sprite('Sprites/personagens/Isac/Isac.png', (1420, 550), camera_group)
-    npc2 = create_sprite('Sprites/personagens/Isac/Isac.png', (100, 550), camera_group)
-    npc3 = create_sprite('Sprites/personagens/Isac/Isac.png', (1320, 1120), camera_group)
+    # Instanciar NPCs
+    npcs = [
+        create_sprite('Sprites/personagens/NPCs/Hellen.png', (1420, 550), camera_group),
+        create_sprite('Sprites/personagens/NPCs/Isac.png', (100, 550), camera_group),
+        create_sprite('Sprites/personagens/NPCs/Mateus.png', (1320, 1120), camera_group)
+    ]
 
-    # Definição do tamanho da caixa de texto e do texto
     text_box_width = 400
     text_box_height = 100
     npc_interaction_text = "Olá! Sou um NPC interativo."
@@ -234,16 +212,16 @@ def main():
                 sys.exit()
             print(f'Posição do jogador: {player.rect.topleft}')
 
-        # Verificação de teclas pressionadas para movimentação
-        player_input(player, wanderley_up, wanderley_down, wanderley_right)
+        # Atualizar entrada do jogador e movimento
+        update_player_input(player, wanderley_up, wanderley_down, wanderley_right)
         move_player(player, player.speed, scenario_rect)
         animate_player(player)
 
-        # Desenho de todos os elementos na tela
+        # Desenhar elementos na tela
         custom_draw(camera_group, player)
 
-        # Exemplo de interação com NPCs
-        for npc in [npc1, npc2, npc3]:
+        # Verificar interação com NPCs
+        for npc in npcs:
             if player.rect.colliderect(npc.rect):
                 draw_text_box(screen, npc_interaction_text, (640, 100), text_box_width, text_box_height)
         
